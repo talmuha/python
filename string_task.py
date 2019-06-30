a2d03cfca9fc9636738385321258b4187c21242c
TALAL <talal.almuhanna@gmail.com>
2019-06-30 19:12:58 +0300

    first commit v 1.0

---
+++ string_task.py
@@ -0,0 +1,16 @@
+print ("Mad libs where libs get Mad Game")
+print ("Let's Start Below")
+print ("insert a full number")
+Number = input()
+print ("insert a single nour")
+Noun_single = input()
+print ("insert a name of a person")
+Name = input()
+print ("insert a verb that a human can do fullfill his hunger or thurst")
+Verb = input()
+
+print ("""
+	It was %s o'clock when I hears a knock at the door.
+	I opened the door and there was a box full of %s with a note saying \"From Mr. %s\".
+	Just as I closed the door I heard a scream \"I WOULD LIKE TO %s A BOWL FULL OF JELLY.\".
+	I froze in place and all I could do was run.\" """ % (Number, Noun_single, Name, Verb)) 
